e4a5a15baf29ac991b54fc4eaa3fd2389e7cf4df
Danvintius <119405034+Danvintius@users.noreply.github.com>
2023-07-03 19:29:24 +0300

    Update JvmComprehension.java

--- JvmComprehension.java
+++ JvmComprehension.java
@@ -1,7 +1,7 @@
 public class JvmComprehension {
 
     public static void main(String[] args) {
-        int i = 1;                      // Примитивная переменная добавляется в Stack Memory
+        int i = 1;                      // Загружается ClassLoader, примитивная переменная добавляется в Stack Memory
         Object o = new Object();        // Оператор new выделит объем памяти в куче, создаётся конструктор в выделенном объеме памяти, объект будет находиться в куче, а ссылка на него в Stack Memory
         Integer ii = 2;                 // Объект будет находиться в куче, а ссылка на него в Stack Memory
         printAll(o, i, ii);             // Под метод создаётся новый фрейм в стеке, ссылка на объект копируется из кучи
@@ -9,7 +9,7 @@ public class JvmComprehension {
     }
 
     private static void printAll(Object o, int i, Integer ii) {
-        Integer uselessVar = 700;                   // Объект будет находиться в куче, а ссылка на него в Stack Memory
-        System.out.println(o.toString() + i + ii);  // Создаётся объект строки в куче, создаётся фрейм, в который будет передано значение выражения
+        Integer uselessVar = 700;                   // Объект будет находиться в куче, а ссылка на него в Stack Memory. Неиспользуемый объект, можен быть удалён сборщиком мусора
+        System.out.println(o.toString() + i + ii);  // Создаётся объект строки в куче; создаётся фрейм, в который будет передано значение выражения
     }
 }
